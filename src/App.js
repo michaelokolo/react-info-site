8aa2ab114ce9c72fa83e0062abe0a8e4d379ff04
Michael Okolo <michaelokolo62@yahoo.com>
2023-03-04 10:07:00 -0500

    react info site

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './Navbar';
+import Main from './Main';
+
+function App() {
+  return (
+    <div className="container">
+        <Navbar />
+        <Main />
+    </div>
+  );
+}
+
+export default App;
